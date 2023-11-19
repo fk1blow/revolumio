7a6e83de09eb36e4a29552218f91140061023dc7
dragos <hedphug@gmail.com>
2023-11-19 16:23:05 +0200

    chore: add socket io client usage explanationo

--- src/lib/socket.io/client.ts
+++ src/lib/socket.io/client.ts
@@ -1,3 +1,6 @@
+// Need this b/c atm the volumio api works with an outdated version of socket.io
+// so we load the exact version of socket.io-client that volumio uses(injected directly into the html)
+// and the expse it to the `window` object
 export function io(endpoint: string) {
   return window.io(endpoint) as ReturnType<typeof import('socket.io-client').io>
 }
